41be6a2ef998ee43f66fc79433482ce558f776f6
jonel123437 <145882424+jonel123437@users.noreply.github.com>
2023-12-10 16:00:54 +0800

    Delete DashboardController.java

--- DashboardController.java
+++
@@ -1,9 +0,0 @@
-package book.system.dashboard;
-
-import javax.swing.*;
-
-public class DashboardController{
-	public DashboardController(JFrame frame, JPanel loginPanel){
-		new DashboardView(frame, loginPanel);
-	}
-}
