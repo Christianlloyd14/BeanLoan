442060a73b6c218070a788f949eb751b517b1264
jonel123437 <145882424+jonel123437@users.noreply.github.com>
2023-12-12 11:13:45 +0800

    fixed title

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import book.system.login.LoginController;
 
 public class Main{
 	public static void main(String args[]){
-		JFrame frame = new JFrame("jonel");
+		JFrame frame = new JFrame("Loan Application");
 		JPanel loginPanel = new JPanel();
 		new LoginController(frame, loginPanel);
 	}
