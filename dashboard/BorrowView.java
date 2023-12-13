a6293a2957305caf371aa2bf0594aea09957fb42
Jonh Mckyle Dayon <148466307+JMKyle1@users.noreply.github.com>
2023-12-13 09:31:29 +0800

    latest fixed problem

--- dashboard/BorrowView.java
+++ dashboard/BorrowView.java
@@ -5,29 +5,25 @@ import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
 
-public class BorrowView{
-	BorrowView(JFrame frame, JPanel welcomePanel, JPanel tabPanel, JPanel borrowPanel){
-		
+public class BorrowView {
+    // Constructor for BorrowView
+    BorrowView(JFrame frame, JPanel welcomePanel, JPanel tabPanel, JPanel borrowPanel) {
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(borrowPanel);
+        frame.repaint();
+        frame.revalidate();
 
-		frame.getContentPane().removeAll();
-		frame.getContentPane().add(borrowPanel);
-		frame.repaint();
-		frame.revalidate();
+        JPanel bluePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        bluePanel.setBounds(0, 0, 312, 516);
+        bluePanel.setBackground(new Color(23, 53, 99));
+        borrowPanel.add(bluePanel);
 
-		
+        JLabel titleLabel = new JLabel("Please fill in the form");
+        titleLabel.setBounds(350, 100, 400, 30);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 25));
+        borrowPanel.add(titleLabel);
 
-		JPanel bluePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
-		bluePanel.setBounds(0, 0, 312, 516);
-		bluePanel.setBackground(new Color(23, 53, 99));
-		borrowPanel.add(bluePanel);
-
-
-		JLabel titleLabel = new JLabel("Please fill in the form");
-		titleLabel.setBounds(350, 100, 400, 30);
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 25));
-		borrowPanel.add(titleLabel);
-
-		JTextField loanAmountField = new JTextField("Loan Amount");
+        JTextField loanAmountField = new JTextField("Loan Amount");
 		loanAmountField.setBounds(350, 150, 250, 30);
 		loanAmountField.addFocusListener(new FocusAdapter() {
 			public void focusGained(FocusEvent e) {
@@ -98,77 +94,86 @@ public class BorrowView{
 		monthlyIncome.setBounds(350, 360, 250, 30);
 		borrowPanel.add(monthlyIncome);
 
-		JButton continueButton = new JButton("Submit");
-		continueButton.setBounds(500, 450, 100, 30);
-		continueButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				continueButton.setEnabled(false);
+        JButton continueButton = new JButton("Submit");
+        continueButton.setBounds(500, 450, 100, 30);
+        continueButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                continueButton.setEnabled(false);
 
-				// Validate if any field is empty
-				if (loanAmountField.getText().isEmpty() || fullNameField.getText().isEmpty() ||
-						numberField.getText().isEmpty() || loanPurpose.getSelectedIndex() == 0 ||
-						profession.getSelectedIndex() == 0 || monthlyIncome.getSelectedIndex() == 0) {
-					JOptionPane.showMessageDialog(frame, "Please fill in all fields.", "Input Error", JOptionPane.ERROR_MESSAGE);
-					continueButton.setEnabled(true);
-					return;
-				}
+                // Validate if the loan amount is a valid numeric value
+                try {
+                    double loanAmount = Double.parseDouble(loanAmountField.getText());
 
-				// Display a confirmation dialog with user input
-				String confirmationMessage = "Loan Amount: " + loanAmountField.getText() + "\n" +
-						"Full Name: " + fullNameField.getText() + "\n" +
-						"Phone Number: " + numberField.getText() + "\n" +
-						"Purpose of Loan: " + loanPurpose.getSelectedItem() + "\n" +
-						"Profession: " + profession.getSelectedItem() + "\n" +
-						"Monthly Income: " + monthlyIncome.getSelectedItem();
+                    // Validate if any field is empty
+                    if (loanAmountField.getText().isEmpty() || fullNameField.getText().isEmpty() ||
+                            numberField.getText().isEmpty() || loanPurpose.getSelectedIndex() == 0 ||
+                            profession.getSelectedIndex() == 0 || monthlyIncome.getSelectedIndex() == 0) {
+                        JOptionPane.showMessageDialog(frame, "Please fill in all fields.", "Input Error", JOptionPane.ERROR_MESSAGE);
+                        continueButton.setEnabled(true);
+                        return;
+                    }
 
-				int confirmDialogResult = JOptionPane.showConfirmDialog(frame, confirmationMessage, "Confirmation", JOptionPane.YES_NO_OPTION);
+                    // Display a confirmation dialog with user input
+                    String confirmationMessage = "Loan Amount: " + loanAmountField.getText() + "\n" +
+                            "Full Name: " + fullNameField.getText() + "\n" +
+                            "Phone Number: " + numberField.getText() + "\n" +
+                            "Purpose of Loan: " + loanPurpose.getSelectedItem() + "\n" +
+                            "Profession: " + profession.getSelectedItem() + "\n" +
+                            "Monthly Income: " + monthlyIncome.getSelectedItem();
 
-				if (confirmDialogResult == JOptionPane.YES_OPTION) {
-					// Proceed with processing the information
-					try {
-						double loanAmount = Double.parseDouble(loanAmountField.getText());
-						String selectedPurpose = (String) loanPurpose.getSelectedItem();
-						String selectedProfession = (String) profession.getSelectedItem();
-						String selectedIncome = (String) monthlyIncome.getSelectedItem();
+                    int confirmDialogResult = JOptionPane.showConfirmDialog(frame, confirmationMessage, "Confirmation", JOptionPane.YES_NO_OPTION);
 
-						// Your existing code for writing to the file goes here...
-						try (FileWriter fileWriter = new FileWriter("masterlist.txt", true)) {
-							String userInformation = fullNameField.getText() + "/" +
-									numberField.getText() + "/" +
-									yearsField.getText() + "/" +
-									selectedPurpose + "/" +
-									selectedProfession + "/" +
-									selectedIncome + "/" +
-									loanAmount + "\n";
-							fileWriter.write(userInformation);
-						} catch (IOException ex) {
-							ex.printStackTrace();
-							JOptionPane.showMessageDialog(frame, "Error writing to file", "Error", JOptionPane.ERROR_MESSAGE);
-						}
+                    if (confirmDialogResult == JOptionPane.YES_OPTION) {
+                        // Proceed with processing the information
 
-						// Clear the fields after submission
-						loanAmountField.setText("");
-						fullNameField.setText("");
-						numberField.setText("");
-						yearsField.setText("");
-						loanPurpose.setSelectedIndex(0);
-						profession.setSelectedIndex(0);
-						monthlyIncome.setSelectedIndex(0);
+                        try {
+                            // Existing code for writing to the file
+                            try (FileWriter fileWriter = new FileWriter("masterlist.txt", true)) {
+                                String userInformation = fullNameField.getText() + "/" +
+                                        numberField.getText() + "/" +
+                                        yearsField.getText() + "/" +
+                                        loanPurpose.getSelectedItem() + "/" +
+                                        profession.getSelectedItem() + "/" +
+                                        monthlyIncome.getSelectedItem() + "/" +
+                                        loanAmount + "\n";
+                                fileWriter.write(userInformation);
+                                JOptionPane.showMessageDialog(frame, "Information submitted successfully!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                                JOptionPane.showMessageDialog(frame, "Error writing to file", "Error", JOptionPane.ERROR_MESSAGE);
+                            }
 
-						// Additional processing or confirmation dialog can go here...
+                            // Clear the fields after submission
+                            loanAmountField.setText("");
+                            fullNameField.setText("");
+                            numberField.setText("");
+                            yearsField.setText("");
+                            loanPurpose.setSelectedIndex(0);
+                            profession.setSelectedIndex(0);
+                            monthlyIncome.setSelectedIndex(0);
 
-					} catch (NumberFormatException ex) {
-						ex.printStackTrace();
-						JOptionPane.showMessageDialog(frame, "Error processing information", "Error", JOptionPane.ERROR_MESSAGE);
-					}
-				} else {
-					// User canceled the confirmation, return to the form
-					continueButton.setEnabled(true);
-				}
-			}
-		});
+                            // Additional processing or confirmation dialog can go here...
 
+                        } catch (NumberFormatException ex) {
+                            ex.printStackTrace();
+                            JOptionPane.showMessageDialog(frame, "Invalid loan amount. Please enter a numeric value.", "Error", JOptionPane.ERROR_MESSAGE);
+                            continueButton.setEnabled(true); // Enable the button to allow the user to correct the input
+                            return; // Exit the actionPerformed method to prevent further processing
+                        } finally {
+                            continueButton.setEnabled(true); // Ensure the button is re-enabled even if an exception occurs
+                        }
+                    } else {
+                        // User canceled the confirmation, return to the form
+                        continueButton.setEnabled(true);
+                    }
+                } catch (NumberFormatException ex) {
+                    ex.printStackTrace();
+                    JOptionPane.showMessageDialog(frame, "Invalid loan amount. Please enter a numeric value.", "Error", JOptionPane.ERROR_MESSAGE);
+                    continueButton.setEnabled(true); // Enable the button to allow the user to correct the input
+                }
+            }
+        });
 
-		borrowPanel.add(continueButton);
-	}
+        borrowPanel.add(continueButton);
+    }
 }
